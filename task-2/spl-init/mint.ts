c974b65b792969aa1065a65b2a336b5b0889973e
Gideon Okorie <giditech14.com>
2025-10-28 23:56:59 +0100

    created the ATA for mint

--- task-2/spl-init/mint.ts
+++ task-2/spl-init/mint.ts
@@ -19,4 +19,30 @@ const token_decimals = 1_000_000; // 1 token with 6 decimals
 
 const mint = new PublicKey("6CYENncbX7Nnn8U2jezP1CtbL9Ymbfc9bPGmdUgVRoFp");
 
-(async () => {})();
+(async () => {
+  try {
+    // Get or create the associated token account for the wallet and mint
+    const ata = await getOrCreateAssociatedTokenAccount(
+      connection,
+      keypair, // payer
+      mint, // mint
+      myWalletPublicKey // owner
+    );
+    console.log("Associated Token Account:", ata.address.toBase58());
+
+    // Mint to ATA
+    /*const mintTx = await mintTo(
+      connection,
+      keypair, // payer
+      mint, // mint
+      ata.address, // destination
+      myWalletPublicKey, // authority
+      10 * token_decimals // amount
+    );*/
+    //console.log("Mint Signature:", mintSignature);
+  } catch (error) {
+    console.error("Error minting tokens:", error);
+  }
+})();
+
+// ATA: 6rWRZMHRHsNYqV3qqC7jDRFBMXbgX5wZ7nfTgmR31aCf
