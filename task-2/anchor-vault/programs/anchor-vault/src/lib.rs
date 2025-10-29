e59413df26eb9d8a36ab13fb4fb60c51f36dcf3b
Gideon Okorie <giditech14.com>
2025-10-29 10:30:10 +0100

    added some tests for vault

--- task-2/anchor-vault/programs/anchor-vault/src/lib.rs
+++ task-2/anchor-vault/programs/anchor-vault/src/lib.rs
@@ -1,16 +1,213 @@
-use anchor_lang::prelude::*;
+#![allow(unexpected_cfgs)]
+#[allow(deprecated)]
+
+use anchor_lang::{prelude::*, system_program::{Transfer, transfer}, InitSpace};
+use anchor_lang::{accounts::signer, prelude::{SystemAccount, borsh::de}};
 
 declare_id!("FGTAsN5tZWpaALtLYPCdNkserZY6WPL7H1MMwhxb5cNS");
 
 #[program]
 pub mod anchor_vault {
     use super::*;
-
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+        ctx.accounts.initialize(ctx.bumps)?;
+        Ok(())
+    }
+
+    pub fn deposit(ctx: Context<Deposit>, amount: u64) -> Result<()> {
+        ctx.accounts.deposit(amount)?;
+        Ok(())
+    }
+
+    pub fn withdraw(ctx: Context<Withdraw>, amount: u64) -> Result<()> {
+        ctx.accounts.withdraw(amount)?;
+        Ok(())
+    }
+
+    pub fn close(ctx: Context<Close>) -> Result<()> {
+        ctx.accounts.close()?;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct Initialize <'info>{
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(
+        init,
+        payer = user,
+        seeds = [b"state", user.key().as_ref()],
+        bump,
+        space = 8 + VaultState::INIT_SPACE // 8 bytes for Anchor discriminator + size of VaultState
+    )]
+    pub vault_state: Account<'info, VaultState>,
+    #[account(
+        mut,
+        seeds = [b"vault", vault_state.key().as_ref()],
+        bump,
+    )]
+    pub vault: SystemAccount<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+impl <'info> Initialize<'info> {
+    pub fn initialize(&mut self, bumps: InitializeBumps) -> Result<()> {
+        let rent_exempt= Rent::get()?.minimum_balance(self.vault_state.to_account_info().data_len());
+
+        let cpi_program= self.system_program.to_account_info();
+
+        let cpi_accounts= Transfer {
+            from: self.user.to_account_info(),
+            to: self.vault.to_account_info(),
+        };
+
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+
+        transfer(cpi_ctx, rent_exempt)?;
+
+        self.vault_state.vault_bump = bumps.vault; 
+        self.vault_state.state_bump = bumps.vault_state;
+
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Deposit <'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(
+        seeds = [b"state", user.key().as_ref()],
+        bump = vault_state.state_bump,
+    )]
+    pub vault_state: Account<'info, VaultState>,
+    #[account(
+        mut,
+        seeds = [b"vault", user.key().as_ref()],
+        bump = vault_state.state_bump,
+    )]
+    pub vault: SystemAccount<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+impl <'info> Deposit<'info> {
+    pub fn deposit(&mut self, amount: u64) -> Result<()> {
+        let cpi_program= self.system_program.to_account_info();
+
+        let cpi_accounts= Transfer {
+            from: self.user.to_account_info(),
+            to: self.vault.to_account_info(),
+        };
+
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+
+        transfer(cpi_ctx, amount)?;
+
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Withdraw <'info>{
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(
+        seeds = [b"state", user.key().as_ref()],
+        bump = vault_state.state_bump,
+    )]
+    pub vault_state: Account<'info, VaultState>,
+    #[account(
+        mut,
+        seeds = [b"vault", user.key().as_ref()],
+        bump = vault_state.state_bump,
+    )]
+    pub vault: SystemAccount<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+impl <'info> Withdraw<'info> {
+    pub fn withdraw (&mut self, amount: u64) -> Result<()> {
+        // Check that the vault has enough balance for rent exemption after withdrawal
+        // Check that the vault has enough balance for user to withdraw the requested amount
+        let cpi_program= self.system_program.to_account_info();
+
+        let cpi_accounts = Transfer {
+            from: self.vault.to_account_info(),
+            to: self.user.to_account_info(),
+        };
+
+        let seeds = &[
+            b"vault".as_ref(),
+            self.vault_state.to_account_info().key.as_ref(),
+            &[self.vault_state.vault_bump],
+        ];
+
+        let signer_seeds = &[&seeds[..]];
+
+        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer_seeds);
+
+        transfer(cpi_ctx, amount)?;
+        
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Close <'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+    #[account(
+        mut,
+        seeds = [b"state", user.key().as_ref()],
+        bump = vault_state.state_bump,
+        close = user
+    )]
+    pub vault_state: Account<'info, VaultState>,
+    #[account(
+        mut,
+        seeds = [b"vault", vault_state.key().as_ref()],
+        bump = vault_state.vault_bump,
+    )]
+    pub vault: SystemAccount<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+impl <'info> Close<'info> {
+    pub fn close(&mut self) -> Result<()> {
+        let cpi_program = self.system_program.to_account_info();
+
+        let cpi_accounts = Transfer {
+            from: self.vault.to_account_info(),
+            to: self.user.to_account_info(),
+        };
+
+        let vault_seeds = &[
+            b"vault".as_ref(),
+            self.vault_state.to_account_info().key.as_ref(),
+            &[self.vault_state.vault_bump],
+        ];
+
+        let signer_seeds = &[&vault_seeds[..]];
+
+        let cpi_ctx = CpiContext::new_with_signer(cpi_program, cpi_accounts, signer_seeds);
+
+        transfer(cpi_ctx, self.vault.lamports())?;
+
+        msg!("Vault closed and remaining funds transferred to user.");
+        Ok(())
+    }
+}
+
+//impl Space for VaultState {
+//    const INIT_SPACE: usize = 
+//                           1 // vault_bump 
+//                            + 1 // state_bump
+//                            + 8; // anchor discriminator (8 bytes, This is what Anchor uses to index your accounts)
+//}
+#[account]
+#[derive(InitSpace)] // This macro does not take into consideration Anchor discriminator size
+pub struct VaultState {
+    vault_bump: u8,
+    state_bump: u8,
+}
--- task-2/anchor-vault/tests/anchor-vault.ts
+++ task-2/anchor-vault/tests/anchor-vault.ts
@@ -1,16 +1,93 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { AnchorVault } from "../target/types/anchor_vault";
+import { LAMPORTS_PER_SOL } from "@solana/web3.js";
 
 describe("anchor-vault", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
 
+  const provider = anchor.AnchorProvider.env(); // Load the default provider from the Anchor environment (usually your local wallet and devnet/localnet RP
+
   const program = anchor.workspace.anchorVault as Program<AnchorVault>;
 
-  it("Is initialized!", async () => {
+  const userPublicKey = anchor.getProvider().wallet.publicKey;
+
+  // Derive the vault state PDA using seeds ["state", user public key]
+  const vaultState = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from("state"), provider.publicKey.toBytes()], // User's public key
+    program.programId
+  )[0];
+
+  // Derive the vault PDA using seeds ["vault", vaultState PDA]
+  const vault = anchor.web3.PublicKey.findProgramAddressSync(
+    [Buffer.from("vault"), vaultState.toBytes()], // Vault state PDA
+    program.programId
+  )[0];
+
+  console.log("Vault State PDA:", vaultState.toBase58());
+  console.log("Vault PDA:", vault.toBase58());
+
+  it("Is initialized with rent exemption!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const tx = await program.methods
+      .initialize()
+      .accountsPartial({
+        vaultState: vaultState,
+        vault: vault,
+        user: provider.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .rpc();
     console.log("Your transaction signature", tx);
+    console.log(
+      "Your vault info",
+      await provider.connection.getAccountInfo(vault)
+    );
+  });
+
+  // Second test: Deposit 2 SOL
+  it("Deposit 2 SOL", async () => {
+    // Calls the `deposit` instruction with 2 SOL (in lamports)
+    // Derive the vault state PDA using seeds ["state", user public key]
+    const vaultState = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("state"), provider.publicKey.toBytes()], // User's public key
+      program.programId
+    )[0];
+
+    // Derive the vault PDA using seeds ["vault", vaultState PDA]
+    const vault = anchor.web3.PublicKey.findProgramAddressSync(
+      [Buffer.from("vault"), vaultState.toBytes()], // Vault state PDA
+      program.programId
+    )[0];
+
+    console.log("Vault State PDA:", vaultState.toBase58());
+    console.log("Vault PDA:", vault.toBase58());
+
+    const tx = await program.methods
+      .deposit(new anchor.BN(2 * LAMPORTS_PER_SOL)) // Wrap 2 SOL in BN for precision. BN means BigNumber which is used to handle large numbers.
+      .accountsPartial({
+        user: provider.publicKey, // Signer
+        vault: vault, // Vault SOL-holding account
+        vaultState: vaultState, // Vault state account
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .rpc(); // Send transaction
+
+    // Log the signature and print vault's updated SOL balance
+    console.log("\nYour transaction signature", tx);
+
+    const vaultInfo = await provider.connection.getAccountInfo(vault);
+
+    if (vaultInfo) {
+      // If vault exists, print balance in SOL
+      console.log(
+        "Your vault balance",
+        vaultInfo.lamports / LAMPORTS_PER_SOL,
+        "SOL"
+      );
+    } else {
+      console.log("Vault account not found");
+    }
   });
 });
